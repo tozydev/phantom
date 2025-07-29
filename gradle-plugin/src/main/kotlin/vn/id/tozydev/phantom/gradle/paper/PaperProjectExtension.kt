6e48e456ecbf1b4d9a4d3c67c26f15e4e00eac4d
Nguyễn Thanh Tân <me@tozydev.id.vn>
2025-07-29 13:35:15 +0700

    refactor(gradle-plugin): make PaperProjectExtension extension aware

--- gradle-plugin/src/main/kotlin/vn/id/tozydev/phantom/gradle/paper/PaperProjectExtension.kt
+++ gradle-plugin/src/main/kotlin/vn/id/tozydev/phantom/gradle/paper/PaperProjectExtension.kt
@@ -1,8 +1,9 @@
 package vn.id.tozydev.phantom.gradle.paper
 
+import org.gradle.api.plugins.ExtensionAware
 import org.gradle.api.provider.Property
 
-interface PaperProjectExtension {
+interface PaperProjectExtension : ExtensionAware {
     val javaVersion: Property<Int>
     val minecraftVersion: Property<String>
 }
