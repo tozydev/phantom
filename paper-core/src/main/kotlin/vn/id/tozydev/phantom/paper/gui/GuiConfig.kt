b329f462527a0e920ad3f135edf424ab3e9195da
Nguyễn Thanh Tân <me@tozydev.id.vn>
2025-08-03 21:28:51 +0700

    feat(paper-core): add GuiConfig and GuiItem classes for GUI configuration and serialization

---
+++ paper-core/src/main/kotlin/vn/id/tozydev/phantom/paper/gui/GuiConfig.kt
@@ -0,0 +1,74 @@
+package vn.id.tozydev.phantom.paper.gui
+
+import org.bukkit.event.inventory.InventoryType
+import org.spongepowered.configurate.ConfigurationNode
+import org.spongepowered.configurate.kotlin.extensions.get
+import org.spongepowered.configurate.objectmapping.ConfigSerializable
+import org.spongepowered.configurate.serialize.SerializationException
+import org.spongepowered.configurate.serialize.TypeSerializer
+import vn.id.tozydev.phantom.paper.inventory.ItemStackBuilder
+import java.lang.reflect.Type
+
+/**
+ * Configuration for a GUI.
+ */
+@ConfigSerializable
+data class GuiConfig(
+    val type: InventoryType = InventoryType.CHEST,
+    val title: String,
+    val structure: List<String>,
+    val actions: Map<String, Char> = emptyMap(),
+    val ingredients: Map<String, String> = emptyMap(),
+)
+
+/** Represents an item in a GUI, with a [default] item and optional [states]. */
+data class GuiItem(
+    val default: ItemStackBuilder,
+    val states: Map<String, ItemStackBuilder> = emptyMap(),
+) {
+    object Serializer : TypeSerializer<GuiItem> {
+        override fun deserialize(
+            type: Type,
+            node: ConfigurationNode,
+        ): GuiItem {
+            val default = node.node("default").get<ItemStackBuilder>()
+            if (default == null) {
+                val item =
+                    node.get<ItemStackBuilder>()
+                        ?: throw SerializationException("GuiItem have wrong format or missing 'default' item")
+                return GuiItem(item)
+            }
+            return GuiItem(
+                default = default,
+                states =
+                    node
+                        .childrenMap()
+                        .filter { it.key != "default" }
+                        .mapValues { (key, value) ->
+                            value.get<ItemStackBuilder>()
+                                ?: throw SerializationException("GuiItem state '$key' is not a valid ItemStackBuilder")
+                        }.mapKeys { it.key.toString() },
+            )
+        }
+
+        override fun serialize(
+            type: Type,
+            obj: GuiItem?,
+            node: ConfigurationNode,
+        ) {
+            if (obj == null) {
+                node.raw(null)
+                return
+            }
+            node.node("default").set(obj.default)
+            obj.states.forEach { (key, value) ->
+                node.node(key).set(value)
+            }
+            if (obj.states.isEmpty()) {
+                node.raw(null) // If no state, we can remove the node
+            } else {
+                node.raw(obj.states) // Otherwise, we keep the state
+            }
+        }
+    }
+}
