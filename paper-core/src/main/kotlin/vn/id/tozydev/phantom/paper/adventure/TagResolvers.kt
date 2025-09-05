75767234e2ab5346e4afc058380fac91e7c3cace
Nguyễn Thanh Tân <me@tozydev.id.vn>
2025-09-05 13:56:28 +0700

    docs(paper-core): add docs for TagResolvers extensions

--- paper-core/src/main/kotlin/vn/id/tozydev/phantom/paper/adventure/TagResolvers.kt
+++ paper-core/src/main/kotlin/vn/id/tozydev/phantom/paper/adventure/TagResolvers.kt
@@ -14,21 +14,36 @@ fun TagResolver(block: TagResolver.Builder.() -> Unit): TagResolver {
     return builder.build()
 }
 
+/**
+ * Creates a placeholder that inserts a MiniMessage string.
+ * The inserted string will impact the rest of the parse process.
+ */
 fun TagResolver.Builder.placeholder(
     key: String,
     value: String,
 ): TagResolver.Builder = this.resolver(Placeholder.parsed(key, value))
 
+/**
+ * Creates a replacement that inserts a component.
+ * This replacement is auto-closing, so its style will not influence the style of following components.
+ */
 fun TagResolver.Builder.placeholder(
     key: String,
     value: ComponentLike,
 ): TagResolver.Builder = this.resolver(Placeholder.component(key, value))
 
+/**
+ * Creates a placeholder that inserts a literal string, without attempting to parse any contained tags.
+ */
 fun TagResolver.Builder.unparsedPlaceholder(
     key: String,
     value: String,
 ): TagResolver.Builder = this.resolver(Placeholder.unparsed(key, value))
 
+/**
+ * Creates a style tag which will modify the style of the component.
+ * This style can be used like other styles.
+ */
 fun TagResolver.Builder.styling(
     key: String,
     vararg style: StyleBuilderApplicable,
